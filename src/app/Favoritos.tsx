c94c5b72ef1ca4b06d1f9a202a689f3fc8875b0d
Aarongarcia32 <rmrr0532@gmail.com>
2026-07-13 21:44:42 -0500

    Add files via upload

--- src/app/Favoritos.tsx
+++ src/app/Favoritos.tsx
@@ -1,7 +1,7 @@
 // src/app/Favoritos.tsx
 import { useState, useEffect } from "react";
 import { Heart, X } from "lucide-react";
-import { obtenerNegocios, BusinessData } from "./data/NegociosEjemplo";
+import { obtenerNegocios, BusinessData } from "./data/negociosEjemplo";
 
 interface FavoritosProps {
   onClose: () => void;
